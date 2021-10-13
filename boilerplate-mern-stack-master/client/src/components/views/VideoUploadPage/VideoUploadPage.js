5e56b878d8a88e87fe353d478e1ed4d3d7799d16
jinhee <jlee04@hubilon.com>
2021-10-13 15:29:43 +0900

    ADD: 노드 서버에 비디오 저장 및 썸네일 생성

--- boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoUploadPage/VideoUploadPage.js
@@ -2,6 +2,7 @@
 import React, { useState} from 'react'
 import { Typography, Button, Form, message, Input, Icon} from 'antd';
 import Dropzone from 'react-dropzone';
+import Axios from 'axios';
 const {TextArea} = Input;
 const {Title} = Typography;
 
@@ -23,6 +24,9 @@ function VideoUploadPage() {
     const [Description, setDescription] = useState("")
     const [Private, setPrivate] = useState(0)
     const [Category, setCategory] = useState("Film & Animation")
+    const [FilePath, setFilePath] = useState("")
+    const [Duration, setDuration] = useState("")
+    const [ThumbnailPath, setThumbnailPath] = useState("")
 
     const onTitleChange = (e) => {
         setVideoTitle(e.currentTarget.value)
@@ -40,18 +44,59 @@ function VideoUploadPage() {
         setCategory(e.currentTarget.value)
     }   
 
+    const onDrop = (files) =>{
+        let formData = new FormData;
+        const config = {
+            header: {'content-type': 'multipart/form-data'}
+        }
+        formData.append('file', files[0])
+        console.log(files);
+
+        Axios.post('/api/video/uploadfiles', formData, config)
+            .then(response =>{
+                if(response.data.success){
+                    // console.log(response.data);
+                    let variable = {
+                        url: response.data.url,
+                        fileName: response.data.fileName
+                    }
+                    
+                    setFilePath(response.data.url)
+
+                    Axios.post('/api/video/thumbnail', variable)
+                        .then(response =>{
+                            if(response.data.success){
+                                console.log(response.data);
+                                setDuration(response.data.fileDuration)
+                                setThumbnailPath(response.data.url)
+                            }else{
+                                alert('썸네일 생성에 실패했습니다.')
+                            }
+                        }).catch(function(error){
+                            console.log(error)
+                            alert('요청 에러')
+                        })
+                }else{
+                    alert('비디오 업로드를 실패했습니다.')
+                }
+        }).catch(function(error){
+            console.log(error)
+            alert('요청 에러')
+        })
+    }
     return (
         <div style={{ maxWidth: '700px', margin:'2rem auto' }}>
             <div style={{ textAlign: 'center', marginBottom: '2rem' }}>
                 <Title level={2}>Upload Video</Title>
             </div>
             <Form onSubmit>
-                <div stype={{'display':'flex', justifyContent:'space-between'}}>
+                <div style={{'display':'flex', justifyContent:'space-between'}}>
                     {/* Drop zone */}
                     <Dropzone
-                    onDrop
-                    multiple
-                    maxSize>
+                        onDrop={onDrop}
+                        multiple={false}
+                        maxSize={1000000000}
+                    >
                     {({getRootProps, getInputProps})=>(
                         <div style={{ width:'300px', height:'240px', border:'1px solid lightgray', display:'flex',
                             alignItems:'center', justifyContent:'center'}} {...getRootProps()}>
@@ -61,9 +106,11 @@ function VideoUploadPage() {
                     )}
                     </Dropzone>
                     {/* Thumnail */}
-                    <div>
-                        <img src alt/>
-                    </div>
+                    {ThumbnailPath &&
+                        <div>
+                            <img src={`http://localhost:5000/${ThumbnailPath}`} alt="thumbnail"/>
+                        </div>
+                    }
                 </div>
                 <br />            
                 <br />
