254b3f9e8b56153622cb25eb346f208dd79e4511
jinhee <jlee04@hubilon.com>
2021-10-20 11:28:32 +0900

    ADD: 댓글 기능 추가

--- boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ boilerplate-mern-stack-master/client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
+import Comment from './Sections/Comment';
 
 function VideoDetailPage(props){
 
@@ -10,6 +11,9 @@ function VideoDetailPage(props){
     const variable = {videoId:videoId}
 
     const [VideoDetail, setVideoDetail] = useState([])
+    
+    const [Comments, setComments] = useState([])
+
     useEffect(()=>{
         console.log(variable);
         Axios.post('/api/video/getVideoDetail', variable)
@@ -21,17 +25,33 @@ function VideoDetailPage(props){
                     alert('비디오 정보를 가져오길 실패했습니다.')
                 }
             })
+
+            Axios.post('/api/comment/getComments', variable)
+                .then(response => {
+                    if(response.data.success){
+                        setComments(response.data.comments)
+
+                        console.log(response.data.comments)
+                    }else{
+                        alert('코멘트 정보를 가져오길 실패했습니다.')
+                    }
+                })
     }, [])
-
+    const refreshFunction = (newComment) => {
+        setComments(Comments.concat(newComment))
+    }
     if(VideoDetail.writer){
-        return (
+        console.log(VideoDetail)
 
+        const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />
+
+        return (
             <Row  gutter={[16, 16]}>
                 <Col lg={18} xs={24}>
                     <div style={{width: '100%', padding:'3rem 4rem'}}>
                         <video style={{width: '100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls/>
                         <List.Item 
-                            actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')} />]}
+                            actions={[ subscribeButton ]}
                         >
     
                         <List.Item.Meta
@@ -41,6 +61,7 @@ function VideoDetailPage(props){
                         />
                         </List.Item>
                         {/* Comments */}
+                        <Comment refreshFunction={refreshFunction} commentLists={Comments} postId={videoId} />
                     </div>
                 </Col>
                 <Col lg={6} xs={24}>
